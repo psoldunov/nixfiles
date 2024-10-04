98a1613306ec4586a7613e194d8431a85d0bca82
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2024-10-04 13:55:27 +0300

    rebuild commit 04/10/2024 13:55:27

--- home-manager/modules/desktop/hyprland.nix
+++ home-manager/modules/desktop/hyprland.nix
@@ -156,7 +156,6 @@ in {
     mime.enable = true;
     portal = {
       enable = true;
-      wlr.enable = true;
       extraPortals = [
         pkgs.xdg-desktop-portal-wlr
         pkgs.xdg-desktop-portal-gtk
