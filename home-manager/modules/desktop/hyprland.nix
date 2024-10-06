13aa72ba8b169e4ecb30fab972f05f25bbd9dd33
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2024-10-06 16:38:52 +0300

    pre-update commit 06/10/2024 16:38:52

--- home-manager/modules/desktop/hyprland.nix
+++ home-manager/modules/desktop/hyprland.nix
@@ -158,7 +158,7 @@ in {
     portal = {
       enable = true;
       extraPortals = [
-        pkgs.xdg-desktop-portal-wlr
+        pkgs.xdg-desktop-portal-hyprland
         pkgs.xdg-desktop-portal-gtk
       ];
       xdgOpenUsePortal = true;
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -213,9 +213,9 @@ in {
   xdg = {
     portal = {
       enable = true;
-      wlr.enable = true;
+      wlr.enable = false;
       extraPortals = [
-        pkgs.xdg-desktop-portal-wlr
+        pkgs.xdg-desktop-portal-hyprland
         pkgs.xdg-desktop-portal-gtk
       ];
       xdgOpenUsePortal = true;
