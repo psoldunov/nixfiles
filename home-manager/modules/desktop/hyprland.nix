0f3d4417803242f9add0090746b887b3d43fc887
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2024-10-17 14:58:05 +0300

    rebuild commit 17/10/2024 14:58:05

--- home-manager/modules/desktop/hyprland.nix
+++ home-manager/modules/desktop/hyprland.nix
@@ -186,7 +186,7 @@ in {
     "${config.xdg.configHome}/hypr/exec.conf" = {
       text = ''
         exec-once = ${config.programs.ags.finalPackage}/bin/ags
-        exec-once = start_static_wallpaper ${wallpaperPath}
+        # exec-once = start_static_wallpaper ${wallpaperPath}
         # exec-once = start_video_wallpaper
         exec-once = ${pkgs.sox}/bin/play ${startupSound}
         exec-once = ${autoStart}
