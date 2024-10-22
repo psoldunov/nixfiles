d8357161444d00e86b1da6c641b5ca85f9a7d753
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2024-10-22 21:13:37 +0300

    rebuild commit 22/10/2024 21:13:37

--- home-manager/modules/desktop/hyprland.nix
+++ home-manager/modules/desktop/hyprland.nix
@@ -44,6 +44,7 @@ in {
   wayland.windowManager.hyprland = {
     enable = true;
     catppuccin.enable = false;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     systemd = {
       enable = true;
       enableXdgAutostart = true;
