222b5fec2457d120d81b2672a1421d284445daf0
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2024-10-23 15:04:00 +0300

    rebuild commit 23/10/2024 15:04:00

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -377,7 +377,7 @@ in {
           OLLAMA_GPU_OVERHEAD = "2147483648";
         };
         volumes = [
-          "/var/lib/ollama:/root/.ollama/_data"
+          "ollama:/root/.ollama"
         ];
       };
       agent = {
