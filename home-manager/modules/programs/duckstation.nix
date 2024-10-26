a3a5fb23d4aa3dab31a56b559bac1ddfa91c2f54
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2024-10-26 16:43:46 +0300

    rebuild commit 26/10/2024 16:43:46

--- home-manager/modules/programs/duckstation.nix
+++ home-manager/modules/programs/duckstation.nix
@@ -1,16 +1,18 @@
 {pkgs, ...}: {
-  "settings.ini" = {
-    source =
-      (pkgs.formats.ini {}).generate "settings.ini"
-      {
-        general = {
-          theme = "dark";
-          font_size = 12;
-          show_tabs_bar = false;
-          tab_width = 30;
-          tab_max_width = 50;
-          tab_min_width = 10;
+  home.file = {
+    "settings.ini" = {
+      source =
+        (pkgs.formats.ini {}).generate "settings.ini"
+        {
+          general = {
+            theme = "dark";
+            font_size = 12;
+            show_tabs_bar = false;
+            tab_width = 30;
+            tab_max_width = 50;
+            tab_min_width = 10;
+          };
         };
-      };
+    };
   };
 }
