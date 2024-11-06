a344af46b70b936179558c1a21af2d78d1bdc937
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2024-11-06 17:19:46 +0200

    rebuild commit 06/11/2024 17:19:46

--- home-manager/modules/programs/duckstation.nix
+++ home-manager/modules/programs/duckstation.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   home.file = {
     "/home/psoldunov/.local/share/duckstation/bios" = {
-      source = "/mnt/Games/Emulation/Settings/DuckStation/bios";
+      path = "/mnt/Games/Emulation/Settings/DuckStation/bios";
     };
   };
 }
