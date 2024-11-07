ce1c07c1d98c7a3055becc780fecc99fac19ebb5
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2024-11-07 11:33:03 +0200

    rebuild commit 07/11/2024 11:33:03

--- home-manager/modules/desktop/hypridle.nix
+++ home-manager/modules/desktop/hypridle.nix
@@ -61,10 +61,10 @@ in {
       # USER AVATAR
 
       image = {
-        path = pkgs.fetchurl {
-          url = "https://avatars.githubusercontent.com/u/69530789";
-          hash = "sha256-aJfYk/ho78bAxfKoaWKhespatwWMzpTljuR1HTSkSDk=";
-        };
+        path = ''${pkgs.fetchurl {
+            url = "https://avatars.githubusercontent.com/u/69530789";
+            hash = "sha256-aJfYk/ho78bAxfKoaWKhespatwWMzpTljuR1HTSkSDk=";
+          }}'';
         size = 100;
         border_color = "$accent";
         position = "0, 75";
