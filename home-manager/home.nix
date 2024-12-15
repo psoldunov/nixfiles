7ff799bb6efb5bb2ae8b27e85a3e9b714c2ac7e5
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2024-12-15 21:29:01 +0200

    rebuild commit 15/12/2024 21:29:00

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -166,7 +166,7 @@ in {
     #    hidePodcasts
     #    shuffle # shuffle+ (special characters are sanitized out of extension names)
     #  ];
-    theme = spicePkgs.themes.catppuccin;
+    theme = spicePkgs.themes.nord;
     colorScheme = "mocha";
   };
 
