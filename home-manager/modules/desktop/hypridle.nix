595597c904d67b44036848de79c1a5980238b1e1
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2024-12-19 14:48:57 +0200

    rebuild commit 19/12/2024 14:48:57

--- home-manager/modules/desktop/hypridle.nix
+++ home-manager/modules/desktop/hypridle.nix
@@ -8,13 +8,15 @@
   hyprlockBin = "${pkgs.hyprlock}/bin/hyprlock";
   hyprctlBin = "${pkgs.hyprland}/bin/hyprctl";
 in {
+  catppuccin.hyprlock = {
+    enable = false;
+    accent = "peach";
+    flavor = "mocha";
+  };
+
   programs.hyprlock = {
     enable = globalSettings.enableHyprland;
-    catppuccin = {
-      enable = false;
-      accent = "peach";
-      flavor = "mocha";
-    };
+
     settings = {
       source = "${config.xdg.configHome}/hypr/mocha.conf";
 
