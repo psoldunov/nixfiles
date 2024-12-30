0385759035c2282aecddd74ab647487704bf597a
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2024-12-30 13:20:43 +0200

    rebuild commit 30/12/2024 13:20:42

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -29,7 +29,7 @@ in {
     enable = true;
     flavor = "mocha";
     accent = "peach";
-    pointerCursor = {
+    cursors = {
       enable = true;
       accent = "dark";
       flavor = "mocha";
