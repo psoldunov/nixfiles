40908c710135f2e86365a9222e655205b00357ab
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2025-01-07 17:59:28 +0200

    rebuild commit 07/01/2025 17:59:28

--- nixos/overlays/supabase-cli.nix
+++ nixos/overlays/supabase-cli.nix
@@ -1,14 +1,15 @@
 self: super: {
-  # sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=
   supabase-cli = super.supabase-cli.overrideAttrs (oldAttrs: rec {
     version = "2.2.1";
-    vendorHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    vendorHash = "sha256-QIt7a+NVd3hubxKOA2kGjouBO5ve7qmIRTIVRpsXmdk=";
+    # vendorHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
 
     src = super.fetchFromGitHub {
       owner = "supabase";
       repo = "cli";
       rev = "v${version}";
       hash = "sha256-m7v31fG3ASbOFGv+IsV5IwnKyr+31i4yAAVpawvFHpo=";
+      # hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA0="
     };
 
     ldflags = [
