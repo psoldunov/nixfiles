de479c4ad4541b1db3c9e4c7bb57583b4760314e
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2025-01-08 22:10:48 +0200

    rebuild commit 08/01/2025 22:10:48

--- home-manager/modules/programs/vscode.nix
+++ home-manager/modules/programs/vscode.nix
@@ -8,12 +8,36 @@
     enable = true;
     enableUpdateCheck = false;
     enableExtensionUpdateCheck = false;
-    extensions = with pkgs;
+    extensions = with pkgs.vscode-extensions;
       [
-        vscode-extensions.continue.continue
-        vscode-extensions.formulahendry.auto-close-tag
-        vscode-extensions.formulahendry.auto-rename-tag
-        vscode-extensions.leonardssh.vscord
+        alefragnani.project-manager
+        bbenoist.nix
+        bradlc.vscode-tailwindcss
+        catppuccin.catppuccin-vsc
+        catppuccin.catppuccin-vsc-icons
+        continue.continue
+        denoland.vscode-deno
+        eamodio.gitlens
+        esbenp.prettier-vscode
+        formulahendry.auto-close-tag
+        formulahendry.auto-rename-tag
+        github.vscode-github-actions
+        gruntfuggly.todo-tree
+        kamadorueda.alejandra
+        leonardssh.vscord
+        mikestead.dotenv
+        ms-azuretools.vscode-docker
+        ms-vscode.cmake-tools
+        ms-vscode.cpptools
+        ms-vscode.cpptools-extension-pack
+        ms-vscode-remote.remote-ssh
+        prisma.prisma
+        tamasfe.even-better-toml
+        vincaslt.highlight-matching-tag
+        vscode-icons-team.vscode-icons
+        vscodevim.vim
+        wix.vscode-import-cost
+        yoavbls.pretty-ts-errors
       ]
       ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
         {
@@ -21,6 +45,12 @@
           publisher = "figma";
           version = "0.4.0";
           sha256 = "3D9z/eHoIAhySLabnhtQCNqSDh7b30XXaBrGeL0zZwA=";
+        }
+        {
+          name = "vscode-sanity";
+          publisher = "sanity-io";
+          version = "0.2.1";
+          sha256 = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
         }
       ];
   };
