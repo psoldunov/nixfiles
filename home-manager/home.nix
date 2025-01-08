2412e0ded1481bb4734f98d1960207c8738e888a
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2025-01-08 21:14:26 +0200

    rebuild commit 08/01/2025 21:14:26

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -280,6 +280,13 @@ in {
     };
   };
 
+  systemd.user.services.mpris-proxy = {
+    description = "Mpris proxy";
+    after = ["network.target" "sound.target"];
+    wantedBy = ["default.target"];
+    serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
+  };
+
   programs = {
     btop.enable = true;
     htop.enable = true;
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -909,13 +909,6 @@ in {
     expressvpn
   ];
 
-  systemd.user.services.mpris-proxy = {
-    description = "Mpris proxy";
-    after = ["network.target" "sound.target"];
-    wantedBy = ["default.target"];
-    serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
-  };
-
   systemd.user.services.activate_expressvpn = {
     description = "Activates ExpressVPN";
     after = ["network.target"];
