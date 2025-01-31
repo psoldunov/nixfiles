c564e95ad5fbe32ad6fdd67af7c26bd7ad876924
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2025-02-01 00:28:15 +0200

    rebuild commit 01/02/2025 00:28:15

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -350,7 +350,7 @@ in {
         Environment = {
           XCURSOR_PATH = "${pkgs.catppuccin-cursors.mochaDark}/share/icons";
           XCURSOR_THEME = "catppuccin-mocha-dark-cursors";
-          # XDG_DATA_DIRS = "${pkgs.tokyo-night-gtk}/share/themes:${pkgs.papirus-icon-theme}/share/icons:$XDG_DATA_DIRS";
+          # XDG_DATA_DIRS = "${catppuccinPackage}/share/themes:${pkgs.papirus-icon-theme}/share/icons:$XDG_DATA_DIRS";
           HYPRCURSOR_THEME = "catppuccin-mocha-dark-cursors";
           HYPRCURSOR_SIZE = "32";
           ICON_THEME = "Papirus-Dark";
