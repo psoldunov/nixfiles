ae798352cbe466a5612dade08079cc75028d01d6
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2025-02-01 00:18:37 +0200

    rebuild commit 01/02/2025 00:18:37

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -18,6 +18,7 @@ in {
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ./modules/mounts.nix
   ];
 
   services.displayManager.sddm.wayland.enable = !globalSettings.enableHyprland;
@@ -25,62 +26,6 @@ in {
   services.desktopManager.plasma6.enable = !globalSettings.enableHyprland;
 
   boot.initrd.kernelModules = ["amdgpu" "nfs"];
-
-  fileSystems."/NVMe" = {
-    device = "/dev/disk/by-label/NVMe";
-    fsType = "ext4";
-    label = "NVMe";
-    options = ["defaults" "x-gvfs-show"];
-  };
-
-  fileSystems."/SATA" = {
-    device = "/dev/disk/by-label/SATA";
-    fsType = "ext4";
-    label = "SATA";
-    options = ["defaults" "x-gvfs-show"];
-  };
-
-  fileSystems."/mnt/Media" = {
-    device = "10.24.24.3:/volume1/Media/";
-    fsType = "nfs";
-    options = ["defaults" "x-gvfs-show" "x-gvfs-symbolic-icon=media-tape-symbolic"];
-  };
-
-  fileSystems."/mnt/Files" = {
-    device = "10.24.24.2:/export/Files/";
-    fsType = "nfs";
-    options = ["defaults" "x-gvfs-show" "x-gvfs-symbolic-icon=file-catalog-symbolic"];
-  };
-
-  fileSystems."/mnt/Documents" = {
-    device = "10.24.24.2:/export/Documents/";
-    fsType = "nfs";
-    options = ["defaults" "x-gvfs-show" "x-gvfs-symbolic-icon=x-office-document-symbolic"];
-  };
-
-  fileSystems."/mnt/Camera" = {
-    device = "10.24.24.3:/volume1/Camera/";
-    fsType = "nfs";
-    options = ["defaults" "x-gvfs-show" "x-gvfs-symbolic-icon=camera-symbolic"];
-  };
-
-  fileSystems."/mnt/Transmission" = {
-    device = "10.24.24.2:/export/transmission";
-    fsType = "nfs";
-    options = ["defaults" "x-gvfs-show" "x-gvfs-symbolic-icon=folder-download-symbolic"];
-  };
-
-  fileSystems."/mnt/SLSKD" = {
-    device = "10.24.24.2:/export/slskd";
-    fsType = "nfs";
-    options = ["defaults" "x-gvfs-show" "x-gvfs-symbolic-icon=folder-download-symbolic"];
-  };
-
-  fileSystems."/mnt/Games" = {
-    device = "10.24.24.3:/volume1/Games";
-    fsType = "nfs";
-    options = ["defaults" "x-gvfs-show" "x-gvfs-symbolic-icon=folder-games-symbolic"];
-  };
 
   console = {
     earlySetup = true;
---
+++ nixos/modules/mounts.nix
@@ -0,0 +1,57 @@
+{...}: {
+  fileSystems."/NVMe" = {
+    device = "/dev/disk/by-label/NVMe";
+    fsType = "ext4";
+    label = "NVMe";
+    options = ["defaults" "x-gvfs-show"];
+  };
+
+  fileSystems."/SATA" = {
+    device = "/dev/disk/by-label/SATA";
+    fsType = "ext4";
+    label = "SATA";
+    options = ["defaults" "x-gvfs-show"];
+  };
+
+  fileSystems."/mnt/Media" = {
+    device = "10.24.24.3:/volume1/Media/";
+    fsType = "nfs";
+    options = ["defaults" "x-gvfs-show" "x-gvfs-symbolic-icon=media-tape-symbolic"];
+  };
+
+  fileSystems."/mnt/Files" = {
+    device = "10.24.24.2:/export/Files/";
+    fsType = "nfs";
+    options = ["defaults" "x-gvfs-show" "x-gvfs-symbolic-icon=file-catalog-symbolic"];
+  };
+
+  fileSystems."/mnt/Documents" = {
+    device = "10.24.24.2:/export/Documents/";
+    fsType = "nfs";
+    options = ["defaults" "x-gvfs-show" "x-gvfs-symbolic-icon=x-office-document-symbolic"];
+  };
+
+  fileSystems."/mnt/Camera" = {
+    device = "10.24.24.3:/volume1/Camera/";
+    fsType = "nfs";
+    options = ["defaults" "x-gvfs-show" "x-gvfs-symbolic-icon=camera-symbolic"];
+  };
+
+  fileSystems."/mnt/Transmission" = {
+    device = "10.24.24.2:/export/transmission";
+    fsType = "nfs";
+    options = ["defaults" "x-gvfs-show" "x-gvfs-symbolic-icon=folder-download-symbolic"];
+  };
+
+  fileSystems."/mnt/SLSKD" = {
+    device = "10.24.24.2:/export/slskd";
+    fsType = "nfs";
+    options = ["defaults" "x-gvfs-show" "x-gvfs-symbolic-icon=folder-download-symbolic"];
+  };
+
+  fileSystems."/mnt/Games" = {
+    device = "10.24.24.3:/volume1/Games";
+    fsType = "nfs";
+    options = ["defaults" "x-gvfs-show" "x-gvfs-symbolic-icon=folder-games-symbolic"];
+  };
+}
