727fd1b95d69f8f04f80442287b0d85c7b672438
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2025-02-04 11:28:09 +0200

    pre-update commit 04/02/2025 11:28:09

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -63,11 +63,11 @@ in {
       substituters = [
         "https://cache.nixos.org/"
         "https://nix-gaming.cachix.org"
-        "https://hyprland.cachix.org"
+        # "https://hyprland.cachix.org"
       ];
       trusted-public-keys = [
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        # "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
     };
   };
