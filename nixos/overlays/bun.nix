60f6006007141298452ce5e834fdd684acf90c62
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2025-02-11 13:26:19 +0200

    rebuild commit 11/02/2025 13:26:19

--- nixos/overlays/bun.nix
+++ nixos/overlays/bun.nix
@@ -9,7 +9,7 @@ self: super: {
       sources = {
         "x86_64-linux" = super.fetchurl {
           url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-linux-x64.zip";
-          hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+          hash = "sha256-P077iv0fhKwqmMBGYciYVh0dNVJ9Awy0Vx6Zt8hfUHk=";
         };
       };
     };
