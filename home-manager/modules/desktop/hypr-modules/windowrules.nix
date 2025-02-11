c1b39a6e1ece57a46d0e6a29d45e9010d3e4b75f
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2025-02-11 14:49:28 +0200

    rebuild commit 11/02/2025 14:49:28

--- home-manager/modules/desktop/hypr-modules/windowrules.nix
+++ home-manager/modules/desktop/hypr-modules/windowrules.nix
@@ -10,8 +10,8 @@
   ];
 
   windowrulev2 = [
-    "float,class:clockify"
-    "pin,class:clockify"
+    "float,class:Clockify"
+    "pin,class:Clockify"
     "fullscreen,class:^steam_app_.*$"
     "noanim,class:^steam_app_.*$"
     "noborder,class:^steam_app_.*$"
