d8face4d46921f2c70955f6e2d8a02ea6a774054
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2025-04-08 17:32:24 +0300

    rebuild commit 08/04/2025 17:32:24

--- home-manager/modules/desktop/hyprland.nix
+++ home-manager/modules/desktop/hyprland.nix
@@ -133,6 +133,7 @@ in {
           disable_hyprland_logo = true;
           disable_splash_rendering = true;
           allow_session_lock_restore = true;
+          anr_missed_pings = 4;
           vrr = 2;
           # vrr = 0;
           key_press_enables_dpms = true;
