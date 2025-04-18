b292fb078f0977947af5a9a3d4dba689ecc80388
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2025-04-18 17:05:54 +0300

    rebuild commit 18/04/2025 17:05:54

--- home-manager/modules/desktop/hyprland.nix
+++ home-manager/modules/desktop/hyprland.nix
@@ -21,10 +21,12 @@
 
   startupSound = ./assets/startup.wav;
 
-  wallpaperPath = pkgs.fetchurl {
-    url = "https://i.imgur.com/iahzUAJ.jpeg";
-    sha256 = "1baa1nrgrs1cz9yf7jiag6w1jznxdj5g18c9i9wjmqc927kb1jy6";
-  };
+  # wallpaperPath = pkgs.fetchurl {
+  #   url = "https://i.imgur.com/iahzUAJ.jpeg";
+  #   sha256 = "1baa1nrgrs1cz9yf7jiag6w1jznxdj5g18c9i9wjmqc927kb1jy6";
+  # };
+
+  wallpaperPath = ./assets/wallpaper.jpg;
 
   autoStart = pkgs.writeShellScript "autostart_applications" ''
     autostart_commands="
