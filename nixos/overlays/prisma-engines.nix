33afc6f1e6f762e9172689b308724173b4a57b46
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2025-04-22 15:14:23 +0300

    rebuild commit 22/04/2025 15:14:23

--- nixos/overlays/prisma-engines.nix
+++ nixos/overlays/prisma-engines.nix
@@ -9,5 +9,8 @@ self: super: {
       rev = version;
       hash = "sha256-moonBNNGWECGPvhyyeHKKAQRXj5lNP0k99JB+1POMUE=";
     };
+
+    useFetchCargoVendor = true;
+    cargoHash = "sha256-GLOGivOH8psE5/M5kYakh9Cab4Xe5Q8isY1c6YDyAB8=";
   });
 }
