a56dc948598c3bbf3fcc02ff1be975889fa87c23
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2025-04-22 13:54:56 +0300

    rebuild commit 22/04/2025 13:54:56

--- home-manager/modules/shell/shell.nix
+++ home-manager/modules/shell/shell.nix
@@ -9,7 +9,7 @@
     GOPATH = "/home/psoldunov/.go";
     MANPAGER = "sh -c 'col -bx | bat -l man -p'";
     DENO_INSTALL = "/home/psoldunov/.deno";
-    PATH = "HOME/.local/bin:/usr/sbin:$HOME/.local/podman/bin:$HOME/.cargo/bin:$DENO_INSTALL/bin:$PATH";
+    PATH = "HOME/.local/bin:/usr/sbin:$HOME/.local/podman/bin:$HOME/.cargo/bin:$DENO_INSTALL/bin:$BUN_PATH:$PATH";
     TERMINAL = "${pkgs.kitty}/bin/kitty";
     TERM = "xterm-256color";
     MAILER = "${pkgs.thunderbird}/bin/thunderbird";
