9dce682251240916d676fa16af6b3620a0e6e54b
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2025-04-24 12:51:48 +0300

    pre-update commit 24/04/2025 12:51:48

--- home-manager/modules/shell/shell.nix
+++ home-manager/modules/shell/shell.nix
@@ -16,7 +16,7 @@
     PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
     PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
     PRISMA_FMT_BINARY = "${pkgs.prisma-engines}/bin/prisma-fmt";
-    PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
+    PRISMA_QUERY_ENGINE_LIBRARY = "${lib.getLib pkgs.prisma-engines}/lib/libquery_engine.node";
   };
 
   programs.lazygit = {
