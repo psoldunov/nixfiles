e7a0d34e22b0015a0d766cadf8e11aec9a8ed7d4
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2025-04-29 15:44:34 +0300

    rebuild commit 29/04/2025 15:44:34

--- nixos/modules/mounts.nix
+++ nixos/modules/mounts.nix
@@ -50,7 +50,7 @@
   };
 
   fileSystems."/mnt/Paperless" = {
-    device = "10.24.24.2:/export/paperless";
+    device = "10.24.24.2:/export/Paperless";
     fsType = "nfs";
     options = ["defaults" "x-gvfs-show" "x-gvfs-symbolic-icon=x-office-document-symbolic"];
   };
