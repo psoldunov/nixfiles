62a3e4b25f9f60a1bae204d1bd18ce9c3ec3c122
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2025-05-18 10:53:24 +0300

    rebuild commit 18/05/2025 10:53:24

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -170,6 +170,16 @@ in {
     mime.enable = true;
     portal = {
       enable = true;
+      config = {
+        common = {
+          "org.freedesktop.portal.FileChooser" = [
+            "gtk"
+          ];
+          "org.freedesktop.portal.Secret" = [
+            "gnome-keyring"
+          ];
+        };
+      };
       extraPortals = with pkgs; [
         xdg-desktop-portal-gtk
       ];
