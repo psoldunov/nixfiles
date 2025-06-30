75f479e9b51ef7c0439bf5e7d4905fdc63ed060a
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2025-06-30 12:05:36 +0300

    rebuild commit 30/06/2025 12:05:36

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -405,6 +405,25 @@ in {
   programs.ssh = {
     enable = true;
     extraConfig = ''
+      Host github.com
+          HostName github.com
+          IdentityFile ~/.ssh/git
+          User git
+          AddKeysToAgent yes
+
+      Host mynixos.com
+          HostName mynixos.com
+          IdentityFile ~/.ssh/git
+          AddKeysToAgent yes
+
+      Host gitlab.com
+          PreferredAuthentications publickey
+          IdentityFile ~/.ssh/git
+          AddKeysToAgent yes
+
+      Host thinkpad.theswisscheese.com
+          ProxyCommand ${pkgs.cloudflared}/bin/cloudflared access ssh --hostname %h
+
       Host *
           IdentityAgent ~/.1password/agent.sock
     '';
