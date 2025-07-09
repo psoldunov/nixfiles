ca52aadcbdc7a1556758304c6039e2b375f972a7
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2025-07-09 18:09:46 +0300

    rebuild commit 09/07/2025 18:09:46

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -686,6 +686,7 @@ in {
       monkeysAudio
       libdiscid
       (writeShellScriptBin "gnome-terminal" "exec -a $0 ${pkgs.kitty}/bin/kitty $@")
+      (writeShellScriptBin "zed" "exec -a $0 ${pkgs.distrobox}/bin/distrobox-enter -n Fedora -- zed $@")
       appimage-run
       wev
       usbutils
