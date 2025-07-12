8a5827bd231aaddd2756d11f4d4eb012fd426a1f
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2025-07-12 16:26:32 +0300

    rebuild commit 12/07/2025 16:26:32

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -594,9 +594,12 @@ in {
   # █▀░ █▄█ █░▀█ ░█░ ▄█
 
   fonts.enableDefaultPackages = true;
-  fonts.fontconfig.defaultFonts.emoji = [
-    "Noto Color Emoji"
-  ];
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts.emoji = [
+      "Noto Color Emoji"
+    ];
+  };
   fonts.packages = with pkgs; [
     roboto
     openmoji-color
