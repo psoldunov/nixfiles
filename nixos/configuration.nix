2c821930afe05ecd473c34c431cd5f75932b1e5c
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2025-10-23 11:35:11 +0300

    pre-update commit 23/10/2025 11:35:11

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -529,7 +529,6 @@ in {
     inputs.catppuccin-vsc.overlays.default
     (import ./overlays/hyprevents.nix)
     (import ./overlays/pedro-raccoon-plymouth.nix)
-    (import ./overlays/hyprprop.nix)
     (import ./overlays/bun.nix)
     # (import ./overlays/supabase-cli.nix)
     (import ./overlays/package-version-server.nix)
