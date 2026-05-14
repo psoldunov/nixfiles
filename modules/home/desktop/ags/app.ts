5b3970756e2d1880ae49e48bdefc1a9e4bd01870
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2026-05-14 20:16:35 +0300

    rebuild commit 14/05/2026 20:16:35

--- modules/home/desktop/ags/app.ts
+++ modules/home/desktop/ags/app.ts
@@ -1,4 +1,5 @@
 import app from "ags/gtk3/app";
+import { Astal, Gdk } from "ags/gtk3";
 import style from "./style.scss";
 import Bar from "./widget/Bar";
 
@@ -6,6 +7,29 @@ app.start({
 	css: style,
 	icons: `${SRC}/assets`,
 	main() {
-		app.get_monitors().map(Bar);
+		const bars = new Map<Gdk.Monitor, Astal.Window>();
+
+		const render = () => {
+			const current = new Set(app.get_monitors());
+			for (const [monitor, win] of bars) {
+				if (!current.has(monitor)) {
+					win.destroy();
+					bars.delete(monitor);
+				}
+			}
+			for (const monitor of current) {
+				if (!bars.has(monitor)) {
+					bars.set(monitor, Bar(monitor) as unknown as Astal.Window);
+				}
+			}
+		};
+
+		render();
+
+		const display = Gdk.Display.get_default();
+		if (display) {
+			display.connect("monitor-added", render);
+			display.connect("monitor-removed", render);
+		}
 	},
 });
